0aef09b97db8fd9256c3eb482f6f7d3916b987ef
Diana S <diana.saginbaeva@gmail.com>
2024-06-01 09:19:41 +0100

    add styles for main menu

--- src/components/header.js
+++ src/components/header.js
@@ -7,13 +7,13 @@ function Header() {
   return (
     <header className="header">
       <img className="header-logo" src={logo} alt="range rover logo" />
-      <div className="main-menu">
-        <a className="menu-title link" href="#">Menu</a>
-        <ul className="menu">
-          <li><a className="link" href="#">vehicles</a></li>
-          <li><a className="link" href="#">Ownership</a></li>
-          <li><a className="link" href="#">Finance</a></li>
-          <li><a className="link" href="#">Explore</a></li>
+      <div className="menu">
+        <a className="menu-title" href="#">Menu</a>
+        <ul className="menu-nav">
+          <li><a className="menu-nav-link" href="#">vehicles</a></li>
+          <li><a className="menu-nav-link" href="#">Ownership</a></li>
+          <li><a className="menu-nav-link" href="#">Finance</a></li>
+          <li><a className="menu-nav-link" href="#">Explore</a></li>
         </ul>
       </div>
       <div className="dealer-info">
@@ -23,5 +23,11 @@ function Header() {
     </header>
   );
 }
+
+let menuTitle = document.querySelectorAll(".menu-title");
+let menuContainer = document.querySelectorAll(".menu-nav");
+menuTitle[0].addEventListener("click", function() {  
+  menuContainer[0].classList.add("opened");
+});
 
 export default Header;
